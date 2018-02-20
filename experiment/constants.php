ff541918718fa9e89a4a00257573e99a856acddf
Shawn Schwartz <shawn.t.schwartz@gmail.com>
2018-02-20 14:28:37 -0800

    Restructured file format and informed consent

--- experiment/constants.php
+++ experiment/constants.php
@@ -1,13 +1,32 @@
 <script>
-  var trial = {
+  var intro_instructions = {
     type: 'instructions',
-    pages: ['Welcome to the experiment. Click next to begin.',
-      'This is the second page of instructions.',
-      'This is the final page.'],
+    pages: ['Welcome to S.K.C. Psych 121 Experiment',
+      'Please follow all instructions throughout the experiment and silence all cell phones.',
+      'Your integrity in the study is important for valid results. Please click next to continue and digitally sign the informed consent.'],
     show_clickable_nav: true
   }
+  
+  var check_consent = function(elem) {
+      if ($('#consent_checkbox').is(':checked')) {
+        return true;
+      } else {
+        alert("If you agree to all of the instructions presented wish to participate in our Psych 121 Experiment, you must check the box next to the statement 'I agree to participate in this Psych 121 Experiment.' All data will be submitted anonymously and no identifying information will be attached to your data file. All results are for Psych 121 purposes only.");
+        return false;
+      }
+      return false;
+    };
+
+  var consent_block = {
+    "type": "html",
+    "force_refresh": true,
+    "url": "src/external_html/simple_consent.html",
+    "cont_btn": "start",
+    "check_fn": check_consent
+  }
+  
   jsPsych.init({
-    timeline: [trial],
+    timeline: [intro_instructions, consent],
     on_finish: function(){ jsPsych.data.displayData(); }
   });
-  </script>
+</script>
--- experiment/header.php
+++ experiment/header.php
@@ -1,9 +1,7 @@
 <!doctype html>
 <html>
   <head>
-    <script src="assets/js/psych/jspsych.js"></script>
-    <script src="assets/js/psych/plugins/jspsych-instructions.js"></script>
-    <script src="assets/js/psych/examples/js/snap.svg-min.js"></script>
-    <link rel="stylesheet" href="assets/js/psych/css/jspsych.css"></link>
-    <link rel="stylesheet" href="assets/js/psych/examples/css/jquery-ui.css"></link>
+    <script src="src/jspsych.js"></script>
+    <script src="src/plugins/jspsych-instructions.js"></script>
+    <link rel="stylesheet" href="src/css/jspsych.css"></link>
   </head>
