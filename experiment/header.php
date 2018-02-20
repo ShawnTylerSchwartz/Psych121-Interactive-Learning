a6d797d38cfa9b3a22fd43cce865611781d19d59
Shawn Schwartz <shawn.t.schwartz@gmail.com>
2018-02-20 14:03:43 -0800

    added the css for the pages

--- experiment/header.php
+++ experiment/header.php
@@ -3,4 +3,5 @@
   <head>
     <script src="assets/js/psych/jspsych.js"></script>
     <script src="assets/js/psych/plugins/jspsych-instructions.js"></script>
+    <link rel="stylesheet" href="assets/js/psych/css/jspsych.css"></link>
   </head>
