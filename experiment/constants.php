95b4235786b163d75871acce70a0ec70db08a348
Shawn Schwartz <shawn.t.schwartz@gmail.com>
2018-02-20 14:37:57 -0800

    testing the new consent form code

--- experiment/constants.php
+++ experiment/constants.php
@@ -8,22 +8,22 @@
   }
   
   var check_consent = function(elem) {
-      if ($('#consent_checkbox').is(':checked')) {
-        return true;
-      } else {
-        alert("If you agree to all of the instructions presented wish to participate in our Psych 121 Experiment, you must check the box next to the statement 'I agree to participate in this Psych 121 Experiment.' All data will be submitted anonymously and no identifying information will be attached to your data file. All results are for Psych 121 purposes only.");
-        return false;
-      }
+    if ($('#consent_checkbox').is(':checked')) {
+      return true;
+    }
+    else {
+      alert("If you agree to all of the instructions presented wish to participate in our Psych 121 Experiment, you must check the box next to the statement 'I agree to participate in this Psych 121 Experiment.' All data will be submitted anonymously and no identifying information will be attached to your data file. All results are for Psych 121 purposes only.");
       return false;
-    };
+    }
+    return false;
+  };
 
   var consent_block = {
-    "type": "html",
-    "force_refresh": true,
-    "url": "src/external_html/simple_consent.html",
-    "cont_btn": "start",
-    "check_fn": check_consent
-  }
+    type:'external-html',
+    url: "src/external_html/simple_consent.html",
+    cont_btn: "start",
+    check_fn: check_consent
+  };
   
   jsPsych.init({
     timeline: [intro_instructions, consent_block],
