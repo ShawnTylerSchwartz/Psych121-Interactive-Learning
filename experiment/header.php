6a74725ac215fc52fde2fa9619ea1aefe21692d6
Shawn Schwartz <shawn.t.schwartz@gmail.com>
2018-02-20 14:07:10 -0800

    adding other js resources

--- experiment/header.php
+++ experiment/header.php
@@ -3,5 +3,7 @@
   <head>
     <script src="assets/js/psych/jspsych.js"></script>
     <script src="assets/js/psych/plugins/jspsych-instructions.js"></script>
+    <script src="assets/js/psych/examples/js/snap.svg-min.js"></script>
     <link rel="stylesheet" href="assets/js/psych/css/jspsych.css"></link>
+    <link rel="stylesheet" href="assets/js/psych/examples/css/jquery-ui.css"></link>
   </head>
