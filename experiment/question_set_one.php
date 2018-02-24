f351a9b6e0d0c5da0e0fd91eff9bdfbeccbd8ca6
Shawn Schwartz <shawn.t.schwartz@gmail.com>
2018-02-24 11:56:15 -0800

    now trying to do timing for stims

--- experiment/question_set_one.php
+++ experiment/question_set_one.php
@@ -1,7 +1,8 @@
 var test_quest_one = {
     type: 'single-stim',
     stimulus: 'src/stim/giphy-downsized-large.gif',
-    choices: [89, 78], // Y or N
+    timing_response: 1000,
+    timing_post_trial: 2000,
     prompt: '<p class="center-content">Have you seen this face before? Y or N.</p>',
     on_finish: function() {
       jsPsych.data.addDataToLastTrial({
