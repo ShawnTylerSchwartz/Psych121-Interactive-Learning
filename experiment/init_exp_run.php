d90b142c8dafad972f15d4bf91826f78690c845b
Shawn Schwartz <shawn.t.schwartz@gmail.com>
2018-02-24 11:40:17 -0800

    trying to fix the single stim

--- experiment/init_exp_run.php
+++ experiment/init_exp_run.php
@@ -1,5 +1,4 @@
 
-
 jsPsych.init({
     display_element: $('#jspsych-target'), timeline: [intro_instructions, consent_block, age_college_block, gender_race_block, first_language_block, breakfast_block, last_cafe_block, hours_slept_block, happiness_block, comfort_block, behere_block, science_block, test_quest_one],
     on_finish: function(){ jsPsych.data.displayData(); }
--- experiment/question_set_one.php
+++ experiment/question_set_one.php
@@ -3,4 +3,9 @@ var test_quest_one = {
     stimulus: 'src/stim/giphy-downsized-large.gif',
     choices: [89, 78], // Y or N
     prompt: '<p class="center-content">Have you seen this face before? Y or N.</p>',
+    on_finish: function() {
+      jsPsych.data.addDataToLastTrial({
+        correct: true
+      });
+    }
   }
