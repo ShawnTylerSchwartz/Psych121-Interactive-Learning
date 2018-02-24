e028f07f57aa56a7b5cb0ceb1d4e1e6217ed3f15
Shawn Schwartz <shawn.t.schwartz@gmail.com>
2018-02-24 12:07:14 -0800

    forget the css for now

--- experiment/question_set_one.php
+++ experiment/question_set_one.php
@@ -13,7 +13,7 @@ var test_quest_one = {
 
 var test_quest_two = {
     type: 'single-stim',
-    stimulus: '<center><h1 style='padding-top:100px;'>This is a test statement</h1></center>',
+    stimulus: '<center><h1>This is a test statement</h1></center>',
     timing_response: 1000,
     timing_post_trial: 2000,
     is_html: true,
