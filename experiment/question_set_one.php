e34b80ad367bcc0d411a8167a2bfbc0fbe6bdd37
Shawn Schwartz <shawn.t.schwartz@gmail.com>
2018-02-24 11:44:33 -0800

    trying a new method for displaying the stim

--- experiment/question_set_one.php
+++ experiment/question_set_one.php
@@ -1,11 +1,8 @@
 var test_quest_one = {
-    type: 'single-stim',
-    stimulus: 'src/stim/giphy-downsized-large.gif',
-    choices: [89, 78], // Y or N
-    prompt: '<p class="center-content">Have you seen this face before? Y or N.</p>',
-    on_finish: function() {
-      jsPsych.data.addDataToLastTrial({
-        correct: true
-      });
-    }
-  }
+      type: 'instructions',
+      pages: [
+        '<iframe src="https://psych.shawntylerschwartz.com/psych100b/experiment/src/resumes/BLS_Group_2/NW_F_UCB.php" width="100%" height="1850px" frameborder="0" scrolling="auto" />',
+        'You will now answer some questions regarding the resume that you just read. Click next to continue.'
+      ],
+      show_clickable_nav: false
+    };
