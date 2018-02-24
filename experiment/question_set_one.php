d3738c3d83d4531b40ce274ce79f19a909ccb891
Shawn Schwartz <shawn.t.schwartz@gmail.com>
2018-02-24 11:48:48 -0800

    truying the video stim

--- experiment/question_set_one.php
+++ experiment/question_set_one.php
@@ -1,8 +1,4 @@
 var test_quest_one = {
-      type: 'instructions',
-      pages: [
-        '<iframe src="https://psych.shawntylerschwartz.com/psych100b/experiment/src/resumes/BLS_Group_2/NW_F_UCB.php" width="100%" height="1850px" frameborder="0" scrolling="auto" />',
-        'You will now answer some questions regarding the resume that you just read. Click next to continue.'
-      ],
-      show_clickable_nav: false
-    };
+  type: 'video',
+  sources: ['src/stim/source.mp4']
+}
