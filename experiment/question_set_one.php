9f85885efddf48d31c68b8e3276e0e78731b6164
Shawn Schwartz <shawn.t.schwartz@gmail.com>
2018-02-24 12:04:27 -0800

    now trying to space the questions properly using css

--- experiment/question_set_one.php
+++ experiment/question_set_one.php
@@ -2,7 +2,7 @@ var test_quest_one = {
     type: 'single-stim',
     stimulus: 'src/stim/giphy-downsized-large.gif',
     timing_response: 1000,
-    timing_post_trial: 2000,
+    timing_post_trial: 1,
     prompt: '<p class="center-content">Have you seen this face before? Y or N.</p>',
     on_finish: function() {
       jsPsych.data.addDataToLastTrial({
@@ -13,7 +13,7 @@ var test_quest_one = {
 
 var test_quest_two = {
     type: 'single-stim',
-    stimulus: '<h1>This is a test statement</h1>',
+    stimulus: '<center><h1 style='padding-top:100px;'>This is a test statement</h1></center>',
     timing_response: 1000,
     timing_post_trial: 2000,
     is_html: true,
