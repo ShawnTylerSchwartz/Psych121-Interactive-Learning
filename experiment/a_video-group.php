18ad6f155f7e9b1c59cb9407bf074b6da8914d31
Shawn Schwartz <shawn.t.schwartz@gmail.com>
2018-02-24 11:36:00 -0800

    tryinying the first single stim

--- experiment/a_video-group.php
+++ experiment/a_video-group.php
@@ -1,5 +1,6 @@
 <script>
   <?php include('intro_consent_const.php'); ?>
   <?php include('demographics_const.php'); ?>
+  <?php include('question_set_one.php'); ?>
   <?php include('init_exp_run.php'); ?>
 </script>
---
+++ experiment/question_set_one.php
@@ -0,0 +1,6 @@
+var test_quest_one = {
+    type: 'single-stim',
+    stimulus: 'src/stim/giphy-downsized-large.gif',
+    choices: [89, 78], // Y or N
+    prompt: '<p class="center-content">Have you seen this face before? Y or N.</p>',
+  }
