8cb6b2533df9686bbe1b6dd3b46c3f5d69c5b153
Shawn Schwartz <shawn.t.schwartz@gmail.com>
2018-02-26 14:03:03 -0800

    Create show_game.php

---
+++ experiment/show_game.php
@@ -0,0 +1,6 @@
+var run_pacman_game = {
+  type: 'instructions-pacman',
+  pages: [
+    '<iframe src="https://psych.shawntylerschwartz.com/psych121/experiment/src/external_html/pacman.html" width="100%" height="600px" frameborder="0" scrolling="auto" />'],
+   show_clickable_nav: true
+};
