a4bd3f713c72d301796f1424e3ff3f33d21bd359
Shawn Schwartz <shawn.t.schwartz@gmail.com>
2018-02-26 15:46:01 -0800

    instructions debug 4

--- experiment/instructions_consts.php
+++ experiment/instructions_consts.php
@@ -6,14 +6,14 @@ var pre_vid_action_instructions = {
     'You will be presented with a video clip that we ask for you to act out as it is being shown. You will be given 3 seconds to prepare before the video starts, indicated by a countdown timer on the screen. Please act out the action as accurately as possible while relating the action with the concept.',
     'After all items and videos have been presented you will be asked a question about each item. Please answer each question to the best of your ability or leave it blank if you are certain that you do not know the answer. When you are ready to being, please click next.'],
   show_clickable_nav: true
-};
+}
 
-var pre_vid_imagine_instructions = {
+var pre_vid_action_instructions_two = {
   type: 'instructions',
   pages: ['The purpose of this study is to measure the difference in the level of encoding between abstract facts versus equally abstract concepts, if both items were associated with related physical actions.',
     'Given a limited amount of time per presented item, we ask that you do not look away from the screen until you are finished.',
     'For each item you will first be presented with a fact or concept followed by a visual representation of a related action.',
-    'You will be presented with a video clip that we ask for you to mentally imagine that you were preforming the action in the video as it is being shown. You will be given 3 seconds to prepare before the video starts, indicated by a countdown timer on the screen. Please mentally imagine yourself acting out the action as accurately as possible while relating the action with the concept.',
+    'You will be presented with a video clip that we ask for you to act out as it is being shown. You will be given 3 seconds to prepare before the video starts, indicated by a countdown timer on the screen. Please act out the action as accurately as possible while relating the action with the concept.',
     'After all items and videos have been presented you will be asked a question about each item. Please answer each question to the best of your ability or leave it blank if you are certain that you do not know the answer. When you are ready to being, please click next.'],
-    show_clickable_nav: true
+  show_clickable_nav: true
 }
