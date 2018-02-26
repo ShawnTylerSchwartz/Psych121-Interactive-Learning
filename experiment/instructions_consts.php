f35a16e4d1c872cfc015c5d567c09b190e6feb1f
Shawn Schwartz <shawn.t.schwartz@gmail.com>
2018-02-26 15:49:29 -0800

    instructions debug 12

--- experiment/instructions_consts.php
+++ experiment/instructions_consts.php
@@ -55,9 +55,3 @@ var pre_quiz_instructions = {
     show_clickable_nav: true
 };
 
-var post_quiz_instructions = {
-  type: 'instructions',
-  pages: ['Thank you for answering the questions. Before you are finished, we ask that you please check off any items from the following list that you already knew before you began this experiment.'
-    'Your honesty is important for the validity of this study. Please choose concepts or facts that you ALREADY KNEW the answer to with CERTAINTY BEFORE BEGINNING THIS EXPERIMENT. When you are ready to being, please click next.'],
-    show_clickable_nav: true
-};
