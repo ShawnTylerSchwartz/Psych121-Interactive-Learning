201e893fa94ae60a2f69bf914460a077ab6494cf
Shawn Schwartz <shawn.t.schwartz@gmail.com>
2018-02-26 08:23:49 -0800

    Added the rest of the stim stills and gifs

--- experiment/video-group_A.php
+++ experiment/video-group_A.php
@@ -1,5 +1,6 @@
 <script>
   <?php include('intro_consent_const.php'); ?>
   <?php include('demographics_const.php'); ?>
+  <?php include('balanced_stimuli.php'); ?>
   <?php include('init_exp_run_A.php'); ?>
 </script>
