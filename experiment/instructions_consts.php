a0979d7671640f2d67f13dbbe9266d7650e45553
Shawn Schwartz <shawn.t.schwartz@gmail.com>
2018-02-26 15:48:20 -0800

    instructions debug 9

--- experiment/instructions_consts.php
+++ experiment/instructions_consts.php
@@ -27,3 +27,13 @@ var pre_still_action_instructions = {
     'After all items and pictures have been presented you will be asked a question about each item. Please answer each question to the best of your ability or leave it blank if you are certain that you do not know the answer. When you are ready to being, please click next.'],
     show_clickable_nav: true
 };
+
+var pre_still_imagine_instructions = {
+  type: 'instructions',
+  pages: ['The purpose of this study is to measure the difference in the level of encoding between abstract facts versus equally abstract concepts, if both items were associated with related physical actions.',
+    'Given a limited amount of time per presented item, we ask that you do not look away from the screen until you are finished.',
+    'For each item you will first be presented with a fact or concept followed by a visual representation of a related action.',
+    'You will be presented with a picture of someone performing an action that we ask for you to mentally imagine that you were preforming the action in the picture as it is being shown. You will be given 3 seconds to prepare before the picture appears, indicated by a blank white screen. Please mentally imagine yourself acting out the action as accurately as possible while relating the action with the concept.',
+    'After all items and pictures have been presented you will be asked a question about each item. Please answer each question to the best of your ability or leave it blank if you are certain that you do not know the answer. When you are ready to being, please click next.'],
+    show_clickable_nav: true
+};
