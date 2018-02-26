75f116387d6e22baee06dc4cb0d24dc5e3226d84
Shawn Schwartz <shawn.t.schwartz@gmail.com>
2018-02-26 15:45:04 -0800

    instructions debug 3

--- experiment/instructions_consts.php
+++ experiment/instructions_consts.php
@@ -6,7 +6,7 @@ var pre_vid_action_instructions = {
     'You will be presented with a video clip that we ask for you to act out as it is being shown. You will be given 3 seconds to prepare before the video starts, indicated by a countdown timer on the screen. Please act out the action as accurately as possible while relating the action with the concept.',
     'After all items and videos have been presented you will be asked a question about each item. Please answer each question to the best of your ability or leave it blank if you are certain that you do not know the answer. When you are ready to being, please click next.'],
   show_clickable_nav: true
-}
+};
 
 var pre_vid_imagine_instructions = {
   type: 'instructions',
