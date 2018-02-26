12951bab3cafc065e7e217d16861906bfc2e779b
Shawn Schwartz <shawn.t.schwartz@gmail.com>
2018-02-26 12:28:29 -0800

    testing the fact questions blocks

--- experiment/balanced_questions.php
+++ experiment/balanced_questions.php
@@ -1,3 +1,35 @@
 <?php include('question_consts.php'); ?>
 
 var fact_question_one = [ "<?php echo $fact_q_one; ?>" ];
+var fact_question_two = [ "<?php echo $fact_q_two; ?>" ];
+var fact_question_three = [ "<?php echo $fact_q_three; ?>" ];
+var fact_question_four = [ "<?php echo $fact_q_four; ?>" ];
+var fact_question_five = [ "<?php echo $fact_q_five; ?>" ];
+var fact_question_six = [ "<?php echo $fact_q_six; ?>" ];
+var fact_question_seven = [ "<?php echo $fact_q_seven; ?>" ];
+var fact_question_eight = [ "<?php echo $fact_q_eight; ?>" ];
+var fact_question_nine = [ "<?php echo $fact_q_nine; ?>" ];
+var fact_question_ten = [ "<?php echo $fact_q_ten; ?>" ];
+var fact_question_eleven = [ "<?php echo $fact_q_eleven; ?>" ];
+var fact_question_twelve = [ "<?php echo $fact_q_twelve; ?>" ];
+var fact_question_thirteen = [ "<?php echo $fact_q_thirteen; ?>" ];
+var fact_question_fourteen = [ "<?php echo $fact_q_fourteen; ?>" ];
+var fact_question_fifteen = [ "<?php echo $fact_q_fifteen; ?>" ];
+var fact_question_sixteen = [ "<?php echo $fact_q_one; ?>" ];
+
+var concept_question_one = [ "<?php echo $concept_q_one; ?>" ];
+var concept_question_two = [ "<?php echo $concept_q_two; ?>" ];
+var concept_question_three = [ "<?php echo $concept_q_three; ?>" ];
+var concept_question_four = [ "<?php echo $concept_q_four; ?>" ];
+var concept_question_five = [ "<?php echo $concept_q_five; ?>" ];
+var concept_question_six = [ "<?php echo $concept_q_six; ?>" ];
+var concept_question_seven = [ "<?php echo $concept_q_seven; ?>" ];
+var concept_question_eight = [ "<?php echo $concept_q_eight; ?>" ];
+
+// Fact Question Blocks
+var fact_question_one_block = {
+  type: 'survey-text',
+  questions: fact_question_one,
+  timing_response: 5000
+};
+
--- experiment/init_exp_run_A.php
+++ experiment/init_exp_run_A.php
@@ -1,4 +1,4 @@
 jsPsych.init({
-    display_element: $('#jspsych-target'), timeline: [fact_one_text, prestim_moving_action, fact_one_stim_moving, fact_two_text],
+    display_element: $('#jspsych-target'), timeline: [fact_one_text, prestim_moving_action, fact_one_stim_moving, fact_two_text, fact_question_one_block],
     on_finish: function(){ jsPsych.data.displayData(); }
 });
