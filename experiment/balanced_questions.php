abf4a464f0b7354c900f4fd36709a1f67baa39ad
Shawn Schwartz <shawn.t.schwartz@gmail.com>
2018-02-26 12:20:45 -0800

    added the questions stim

---
+++ experiment/balanced_questions.php
@@ -0,0 +1,3 @@
+<?php include('question_consts.php'); ?>
+
+var fact_question_one = [ <?php echo $fact_q_one; ?> ];
--- experiment/video-group_A.php
+++ experiment/video-group_A.php
@@ -2,5 +2,6 @@
   <?php include('intro_consent_const.php'); ?>
   <?php include('demographics_const.php'); ?>
   <?php include('balanced_stimuli.php'); ?>
+  <?php include('balanced_questions.php'); ?>
   <?php include('init_exp_run_A.php'); ?>
 </script>
