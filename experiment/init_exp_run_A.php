1afc62ca74ad905a0f98aa5ced9732a265f6e22c
Shawn Schwartz <shawn.t.schwartz@gmail.com>
2018-02-26 08:46:11 -0800

    balanced stim debugging 5

--- experiment/init_exp_run_A.php
+++ experiment/init_exp_run_A.php
@@ -1,4 +1,4 @@
 jsPsych.init({
-    display_element: $('#jspsych-target'), timeline: [intro_instructions, consent_block, age_college_block, gender_race_block, first_language_block, breakfast_block, last_cafe_block, hours_slept_block, happiness_block, comfort_block, behere_block, science_block],
+    display_element: $('#jspsych-target'), timeline: [intro_instructions, consent_block, age_college_block, gender_race_block, first_language_block, breakfast_block, last_cafe_block, hours_slept_block, happiness_block, comfort_block, behere_block, science_block, fact_one_text, prestim_moving_action, fact_one_stim_moving],
     on_finish: function(){ jsPsych.data.displayData(); }
 });
