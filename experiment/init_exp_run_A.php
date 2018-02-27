13202335fa822f254feb4ed2080aa4e414811212
Shawn Schwartz <shawn.t.schwartz@gmail.com>
2018-02-26 16:23:11 -0800

    testing writing of exp data

--- experiment/init_exp_run_A.php
+++ experiment/init_exp_run_A.php
@@ -1,4 +1,25 @@
+function random() {
+  var x = Math.random();
+  return x;
+};
+    
+var bls_output = "BLS_GROUP_P-4-4-";
+var rand_output = random();
+var the_dot = ".";
+var csv_name = "csv";
+    
+var afranco = bls_output + rand_output + the_dot + csv_name;
+    
+function saveData(filename, filedata){
+  $.ajax({
+    type:'post',
+    cache: false,
+    url: 'save_data.php',
+    data: {filename: filename, filedata: filedata}
+  });
+};
+
 jsPsych.init({
     display_element: $('#jspsych-target'), timeline: [run_pacman_game, final_yn_facts_concepts_block],
-    on_finish: function(){ jsPsych.data.displayData(); }
+    on_finish: function(data){ saveData(afranco, jsPsych.data.dataAsCSV()); }
 });
