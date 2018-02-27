234bf02b8b8b27e5adc23dd031324c729f5c5313
Shawn Schwartz <shawn.t.schwartz@gmail.com>
2018-02-26 16:09:27 -0800

    testing other method for pacman

--- experiment/show_game.php
+++ experiment/show_game.php
@@ -1,5 +1,5 @@
 var run_pacman_game = {
-  type: 'instructions-pacman',
+  type: 'instructions',
   pages: [
     '<iframe src="https://psych.shawntylerschwartz.com/psych121/experiment/src/external_html/pacman.html" width="100%" height="600px" frameborder="0" scrolling="auto" />'],
    show_clickable_nav: true
