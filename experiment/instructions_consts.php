bdf879c82459e97ab8e5beb05f2d095308fd9bee
Shawn Schwartz <shawn.t.schwartz@gmail.com>
2018-03-06 03:36:56 -0800

    fix for mispellings on instructions consts

--- experiment/instructions_consts.php
+++ experiment/instructions_consts.php
@@ -4,7 +4,7 @@ var pre_vid_action_instructions = {
     'Given a limited amount of time per presented item, we ask that you do not look away from the screen until you are finished.',
     'For each item you will first be presented with a fact or concept followed by a visual representation of a related action.',
     'You will be presented with a video clip that we ask for you to act out as it is being shown. You will be given 3 seconds to prepare before the video starts, indicated by a countdown timer on the screen. Please act out the action as accurately as possible while relating the action with the concept.',
-    'After all items and videos have been presented you will be asked a question about each item. Please answer each question to the best of your ability or leave it blank if you are certain that you do not know the answer. When you are ready to being, please click next.'],
+    'After all items and videos have been presented you will be asked a question about each item. Please answer each question to the best of your ability or leave it blank if you are certain that you do not know the answer. When you are ready to begin, please click next.'],
   show_clickable_nav: true
 };
 
@@ -14,21 +14,21 @@ var pre_vid_imagine_instructions = {
     'Given a limited amount of time per presented item, we ask that you do not look away from the screen until you are finished.',
     'For each item you will first be presented with a fact or concept followed by a visual representation of a related action.',
     'You will be presented with a video clip that we ask for you to mentally imagine that you were preforming the action in the video as it is being shown. You will be given 3 seconds to prepare before the video starts, indicated by a countdown timer on the screen. Please mentally imagine yourself acting out the action as accurately as possible while relating the action with the concept.',
-    'After all items and videos have been presented you will be asked a question about each item. Please answer each question to the best of your ability or leave it blank if you are certain that you do not know the answer. When you are ready to being, please click next.'],
+    'After all items and videos have been presented you will be asked a question about each item. Please answer each question to the best of your ability or leave it blank if you are certain that you do not know the answer. When you are ready to begin, please click next.'],
     show_clickable_nav: true
 };
 
 var pre_still_action_instructions = {
   type: 'instructions',
   pages: ['You will now be presented with a picture of someone performing an action that we ask for you to act out as it is being shown. You will be given 3 seconds to prepare before the picture appears, indicated by a blank white screen. Please act out the action as accurately as possible while relating the action with the concept. Arrows have been provided for your assistance.',
-    'After all items and pictures have been presented you will be asked a question about each item. Please answer each question to the best of your ability or leave it blank if you are certain that you do not know the answer. When you are ready to being, please click next.'],
+    'After all items and pictures have been presented you will be asked a question about each item. Please answer each question to the best of your ability or leave it blank if you are certain that you do not know the answer. When you are ready to begin, please click next.'],
     show_clickable_nav: true
 };
 
 var pre_still_imagine_instructions = {
   type: 'instructions',
   pages: ['You will now be presented with a picture of someone performing an action that we ask for you to mentally imagine that you were preforming the action in the picture as it is being shown. You will be given 3 seconds to prepare before the picture appears, indicated by a blank white screen. Please mentally imagine yourself acting out the action as accurately as possible while relating the action with the concept. Arrows have been provided for your assistance.',
-    'After all items and pictures have been presented you will be asked a question about each item. Please answer each question to the best of your ability or leave it blank if you are certain that you do not know the answer. When you are ready to being, please click next.'],
+    'After all items and pictures have been presented you will be asked a question about each item. Please answer each question to the best of your ability or leave it blank if you are certain that you do not know the answer. When you are ready to begin, please click next.'],
     show_clickable_nav: true
 };
 
@@ -36,8 +36,8 @@ var pre_controlled_instructions = {
   type: 'instructions',
   pages: ['The purpose of this study is to measure the difference in the level of encoding between abstract facts versus equally abstract concepts, if both items were associated with related physical actions.',
     'Given a limited amount of time per presented item, we ask that you do not look away from the screen until you are finished.',
-    'For each item you will first be presented with a fact or concept followed by a visual representation of a related action.',
-    'Try to relate the action with the concept. After all items and images are presented, you will be asked a question about each item. Please answer each question to the best of your ability or leave it blank if you are certain that you do not know the answer. When you are ready to being, please click next.'],
+    'For each item you will first be presented with a fact or concept.',
+    'After all items and images are presented, you will be asked a question about each item. Please answer each question to the best of your ability or leave it blank if you are certain that you do not know the answer. When you are ready to begin, please click next.'],
     show_clickable_nav: true
 };
 
@@ -45,21 +45,21 @@ var pre_game_instructions = {
   type: 'instructions',
   pages: ['You will now play a short game before moving on.',
     'The game will appear and you will play until the countdown timer indicates for you to click next and move on. DO NOT click next until the timer instructs you to do so...just keep playing.',
-    'When you are ready to being, please click next.'],
+    'When you are ready to begin, please click next.'],
     show_clickable_nav: true
 };
 
 var pre_quiz_instructions = {
   type: 'instructions',
   pages: ['You will now be asked questions regarding the items that were just presented to you.',
-    'Please answer each question to the best of your ability or leave it blank if you are certain that you do not know the answer. When you are ready to being, please click next.'],
+    'Please answer each question to the best of your ability or leave it blank if you are certain that you do not know the answer. When you are ready to begin, please click next.'],
     show_clickable_nav: true
 };
 
 var post_quiz_instructions = {
   type: 'instructions',
   pages: ['Thank you for answering the questions. Before you are finished, we ask that you please check off any items from the following list that you already knew before you began this experiment. Once you complete these questions a solid white screen will indicate the end of the experiment. At this time you can close down Google Chrome.',
-    'Your honesty is important for the validity of this study. Please choose concepts or facts that you ALREADY KNEW the answer to with CERTAINTY BEFORE BEGINNING THIS EXPERIMENT. When you are ready to being, please click next.'],
+    'Your honesty is important for the validity of this study. Please choose concepts or facts that you ALREADY KNEW the answer to with CERTAINTY BEFORE BEGINNING THIS EXPERIMENT. When you are ready to begin, please click next.'],
     show_clickable_nav: true
 };
 
